c89e649d11dec625ffb9a7be24dc7ff5a48502d2
Dimudev <dimudeveloper@gmail.com>
2026-02-08 19:12:44 -0500

    fix: styles for Header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,13 +1,19 @@
-import { User } from 'lucide-react'
+import { ChevronDown, User } from 'lucide-react'
 import styles from './header.module.css'
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <span className={styles.logo}>Spybee</span>
-      <div>
-        <User />
-        <p>Marco</p>
+      <div className={styles.user_profile}>
+        <div className={styles.icon_container}>
+          <User size={18} />
+        </div>
+        <div>
+          <p className={styles.user_name}>Marco</p>
+          <p className={styles.user_role} >Administrador</p>
+        </div>
+        <ChevronDown size={20} color='white ' />
       </div>
     </header>
   )
